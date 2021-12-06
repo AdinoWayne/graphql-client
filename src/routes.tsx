407eab73b6b30b9a5dcc50b723d09e6c11b6ab67
tqtrung <tqtrung@humaxdigital.com>
2021-12-06 11:11:17 +0700

    [2]

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import managementRoutes from 'views/management/managementRoutes';
-import statusEventRoutes from 'views/statusEvent/statusEventRoutes';
-import maintenanceRoutes from 'views/maintenance/maintenanceRoutes';
 const routes = [
-    ...managementRoutes,
-    ...statusEventRoutes,
-    ...maintenanceRoutes
+    ...managementRoutes
 ]
 
 export default routes;
